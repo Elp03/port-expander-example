16cbbcd9e0889ec90ecf76f6338f1adfdb38d6e6
Athamantis <helle@grasmo.no>
2026-03-09 15:26:23 +0100

    import correct packages

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,37 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+// use port_expander::dev::pcal9714;
+use embassy_nrf::{
+    bind_interrupts, peripherals, spim,
+    spis::{self, MODE_0},
+};
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    // Communication for IO Expander
+    SERIAL2 => spim::InterruptHandler<peripherals::SERIAL2>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    let _p = embassy_nrf::init(Default::default());
+    let p = embassy_nrf::init(Default::default());
+
+    let mut config_spim = spim::Config::default();
+    config_spim.frequency = spim::Frequency::M1;
+    config_spim.mode = MODE_0;
+    config_spim.bit_order = spim::BitOrder::MSB_FIRST;
+
+    defmt::info!("Initialising SPI");
+
+    let sck = unsafe { p.P0_12.clone_unchecked() };
+    let mosi = unsafe { p.P0_14.clone_unchecked() };
+    let miso = unsafe { p.P0_16.clone_unchecked() };
+
+    let mut spim = spim::Spim::new(p.SERIAL2, Irqs, sck, miso, mosi, config_spim);
+
+    // let pcal9714 = port_expander::PCAL9714::new_PCAL9714(spim, false);
+
     loop {
         defmt::info!("Blink");
         Timer::after(Duration::from_millis(100)).await;
