0ac8558dfa8f752692c8d1a5c08976433d78dc07
Athamantis <helle@grasmo.no>
2026-03-16 16:17:51 +0100

    add high and low pins

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,8 @@ async fn main(_spawner: Spawner) -> ! {
 
     let mut spim = spim::Spim::new(p.SERIAL2, Irqs, sck, miso, mosi, config_spim);
 
+    defmt::info!("SPI initialized");
+
     // let spi_bus = Mutex::new(spim);
     // let spi_bus = SPI_BUS.init(spi_bus);
     let spi_bus = NoopMutex::new(RefCell::new(spim));
@@ -51,10 +53,37 @@ async fn main(_spawner: Spawner) -> ! {
     let cs_pin1 = Output::new(p.P0_27, Level::High, OutputDrive::Standard);
     let spi_dev1 = SpiDevice::new(spi_bus, cs_pin1);
 
-    let pcal9714 = port_expander::PCAL9714::new_PCAL9714(spi_dev1, false);
+    defmt::info!("SPI Device initialized");
+
+    let mut pcal9714 = port_expander::PCAL9714::new_PCAL9714(spi_dev1, false);
+    defmt::info!("Port expander initialized");
+
+    let pins = pcal9714.split();
+    defmt::info!("Split succsessfully");
+
+    let mut pin1_2 = match pins.gp0_0.into_output() {
+        Ok(t) => {
+            defmt::info!("Init succsessfully");
+            t
+        }
+        Err(e) => {
+            defmt::info!("Failed to fetch pin 1_2. Error ");
+            loop {}
+        }
+    };
+    // let mut pin1_3 = pins.gp1_3.into_output().unwrap();
+    // let mut pin1_4 = pins.gp1_4.into_output().unwrap();
+    defmt::info!("all pins initialized");
 
     loop {
+        _ = pin1_2.set_high();
+        // _ = pin1_3.set_high();
+        // _ = pin1_4.set_high();
         defmt::info!("Blink");
-        Timer::after(Duration::from_millis(100)).await;
+        Timer::after(Duration::from_millis(2000)).await;
+        _ = pin1_2.set_low();
+        // _ = pin1_3.set_low();
+        // _ = pin1_4.set_low();
+        Timer::after(Duration::from_millis(2000)).await;
     }
 }
