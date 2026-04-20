bf697f8d118687edcade4f09db33ea2c74812b8d
Athamantis <helle@grasmo.no>
2026-04-20 14:28:08 +0200

    testingtesting

--- src/main.rs
+++ src/main.rs
@@ -29,61 +29,96 @@ static SPI_BUS: StaticCell<NoopMutex<RefCell<spim::Spim>>> = StaticCell::new();
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    let p = embassy_nrf::init(Default::default());
+    let mut p = embassy_nrf::init(Default::default());
+    // Timer::after_secs(1).await;
 
     let mut config_spim = spim::Config::default();
-    config_spim.frequency = spim::Frequency::M1;
+    config_spim.frequency = spim::Frequency::M4;
     config_spim.mode = MODE_0;
     config_spim.bit_order = spim::BitOrder::MSB_FIRST;
 
     defmt::info!("Initialising SPI");
+    // Timer::after_secs(1).await;
 
-    let sck = unsafe { p.P0_12.clone_unchecked() };
-    let mosi = unsafe { p.P0_14.clone_unchecked() };
-    let miso = unsafe { p.P0_16.clone_unchecked() };
+    let sck = p.P0_12;
+    let mosi = p.P0_14;
+    let miso = p.P0_16;
 
     let mut spim = spim::Spim::new(p.SERIAL2, Irqs, sck, miso, mosi, config_spim);
 
     defmt::info!("SPI initialized");
-
-    // let spi_bus = Mutex::new(spim);
-    // let spi_bus = SPI_BUS.init(spi_bus);
+    // Timer::after_secs(1).await;
+    //
     let spi_bus = NoopMutex::new(RefCell::new(spim));
     let spi_bus = SPI_BUS.init(spi_bus);
     let cs_pin1 = Output::new(p.P0_27, Level::High, OutputDrive::Standard);
     let spi_dev1 = SpiDevice::new(spi_bus, cs_pin1);
 
     defmt::info!("SPI Device initialized");
+    // Timer::after_secs(1).await;
 
     let mut pcal9714 = port_expander::PCAL9714::new_PCAL9714(spi_dev1, false);
     defmt::info!("Port expander initialized");
+    // Timer::after_secs(1).await;
 
     let pins = pcal9714.split();
     defmt::info!("Split succsessfully");
+    Timer::after_secs(1).await;
 
-    let mut pin1_2 = match pins.gp0_0.into_output() {
-        Ok(t) => {
-            defmt::info!("Init succsessfully");
-            t
-        }
-        Err(e) => {
-            defmt::info!("Failed to fetch pin 1_2. Error ");
-            loop {}
-        }
-    };
-    // let mut pin1_3 = pins.gp1_3.into_output().unwrap();
-    // let mut pin1_4 = pins.gp1_4.into_output().unwrap();
+    let mut pin0_0 = pins.gp0_0.into_output().expect("Pin 0.0 failed");
+    let mut pin0_1 = pins.gp0_1.into_output().expect("Pin 0.1 failed");
+    let mut pin0_2 = pins.gp0_2.into_output().expect("Pin 0.2 failed");
+    let mut pin0_3 = pins.gp0_3.into_output().expect("Pin 0.3 failed");
+    let mut pin0_4 = pins.gp0_4.into_output().expect("Pin 0.4 failed");
+    let mut pin0_5 = pins.gp0_5.into_output().expect("Pin 0.5 failed");
+    let mut pin0_6 = pins.gp0_6.into_output().expect("Pin 0.6 failed");
+    let mut pin0_7 = pins.gp0_7.into_output().expect("Pin 0.7 failed");
+    let mut pin1_0 = pins.gp1_0.into_output().expect("Pin 1.0 failed");
+    let mut pin1_1 = pins.gp1_1.into_output().expect("Pin 1.1 failed");
+    let mut pin1_2 = pins.gp1_2.into_output().expect("Pin 1.2 failed");
+    let mut pin1_3 = pins.gp1_3.into_output().expect("Pin 1.3 failed");
+    let mut pin1_4 = pins.gp1_4.into_output().expect("Pin 1.4 failed");
+    let mut pin1_5 = pins.gp1_5.into_output().expect("Pin 1.4 failed");
     defmt::info!("all pins initialized");
+    Timer::after_secs(1).await;
 
     loop {
-        _ = pin1_2.set_high();
-        // _ = pin1_3.set_high();
-        // _ = pin1_4.set_high();
+        // pin0_0.set_high().expect("Pin 0.0 failed");
+        // pin0_1.set_high().expect("Pin 0.1 failed");
+        // pin0_2.set_high().expect("Pin 0.2 failed");
+        // pin0_3.set_high().expect("Pin 0.3 failed");
+        // pin0_4.set_high().expect("Pin 0.4 failed");
+        // pin0_5.set_high().expect("Pin 0.5 failed");
+        // pin0_6.set_high().expect("Pin 0.6 failed");
+        // pin0_7.set_high().expect("Pin 0.7 failed");
+        // pin1_0.set_high().expect("Pin 1.0 failed");
+        pin1_5.set_high().expect("Pin 1.1 failed");
+        Timer::after_millis(1).await;
+        pin1_4.set_high().expect("Pin 1.2 failed");
+        pin1_3.set_high().expect("Pin 1.3 failed");
+        pin1_2.set_high().expect("Pin 1.4 failed");
+        pin1_1.set_high().expect("Pin 1.5 failed");
+        // pin1_5.set_high()unwrap();
         defmt::info!("Blink");
-        Timer::after(Duration::from_millis(2000)).await;
-        _ = pin1_2.set_low();
-        // _ = pin1_3.set_low();
-        // _ = pin1_4.set_low();
-        Timer::after(Duration::from_millis(2000)).await;
+        // Timer::after_secs(2).await;
+        Timer::after(Duration::from_millis(400)).await;
+        // pin0_0.set_low().expect("Pin 0.0 failed");
+        // pin0_1.set_low().expect("Pin 0.1 failed");
+        // pin0_2.set_low().expect("Pin 0.2 failed");
+        // pin0_3.set_low().expect("Pin 0.3 failed");
+        // pin0_4.set_low().expect("Pin 0.4 failed");
+        // pin0_5.set_low().expect("Pin 0.5 failed");
+        // pin0_6.set_low().expect("Pin 0.6 failed");
+        // pin0_7.set_low().expect("Pin 0.7 failed");
+        // pin1_0.set_low().expect("Pin 1.0 failed");
+        pin1_5.set_low().expect("Pin 1.1 failed");
+        Timer::after_millis(1).await;
+        pin1_4.set_low().expect("Pin 1.2 failed");
+        pin1_3.set_low().expect("Pin 1.3 failed");
+        pin1_2.set_low().expect("Pin 1.4 failed");
+        pin1_1.set_low().expect("Pin 1.5 failed");
+        // pin1_5.set_low().nwrap();
+        // Timer::after(Duration::from_millis(2000)).await;
+        Timer::after(Duration::from_millis(200)).await;
     }
 }
