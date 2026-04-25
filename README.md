0e29d11c78d6d0a44b43e5d2ca2eed94bc869344
Athamantis <helle@grasmo.no>
2026-04-25 12:22:51 +0200

    test for pullup

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Test for checking the PCAL9471
+Test for checking the PCAL9714
 
-
+The tests are done with using a nrf9151 and the PCAL9714. 
+Testing the signals a logic analyser was used. 
+The PCAL9714 was soldered to a breakout board which slotted into a breadboard for easy accesing. (And also because there was something wrong with my custom PCB :02cry:)
 
 Implemented | Hardware tested | Branch | Note 
 --- |--- |--- |--- |
--- src/main.rs
+++ src/main.rs
@@ -48,65 +48,61 @@ async fn main(_spawner: Spawner) -> ! {
     let mut pcal9714 = port_expander::Pcal9714::new_pcal9714(spi_dev1, false);
     defmt::info!("Port expander initialized");
 
-    let pins = pcal9714.split();
+    let mut pins = pcal9714.split();
     defmt::info!("Split succsessfully");
     Timer::after_secs(1).await;
 
-    let mut pin0_0 = pins.gp0_0.into_output().expect("Pin 0.0 failed");
-    let mut pin0_1 = pins.gp0_1.into_output().expect("Pin 0.1 failed");
-    let mut pin0_2 = pins.gp0_2.into_output().expect("Pin 0.2 failed");
-    let mut pin0_3 = pins.gp0_3.into_output().expect("Pin 0.3 failed");
-    let mut pin0_4 = pins.gp0_4.into_output().expect("Pin 0.4 failed");
-    let mut pin0_5 = pins.gp0_5.into_output().expect("Pin 0.5 failed");
-    let mut pin0_6 = pins.gp0_6.into_output().expect("Pin 0.6 failed");
-    let mut pin0_7 = pins.gp0_7.into_output().expect("Pin 0.7 failed");
-    let mut pin1_0 = pins.gp1_0.into_output().expect("Pin 1.0 failed");
-    let mut pin1_1 = pins.gp1_1.into_output().expect("Pin 1.1 failed");
-    let mut pin1_2 = pins.gp1_2.into_output().expect("Pin 1.2 failed");
-    let mut pin1_3 = pins.gp1_3.into_output().expect("Pin 1.3 failed");
-    let mut pin1_4 = pins.gp1_4.into_output().expect("Pin 1.4 failed");
-    let mut pin1_5 = pins.gp1_5.into_output().expect("Pin 1.4 failed");
+    // let mut pin0_0 = pins.gp0_0.into_output().expect("Pin 0.0 failed");
+    // let mut pin0_1 = pins.gp0_1.into_output().expect("Pin 0.1 failed");
+    // let mut pin0_2 = pins.gp0_2.into_output().expect("Pin 0.2 failed");
+    // let mut pin0_3 = pins.gp0_3.into_output().expect("Pin 0.3 failed");
+    // let mut pin0_4 = pins.gp0_4.into_output().expect("Pin 0.4 failed");
+    // let mut pin0_5 = pins.gp0_5.into_output().expect("Pin 0.5 failed");
+    // let mut pin0_6 = pins.gp0_6.into_output().expect("Pin 0.6 failed");
+    // let mut pin0_7 = pins.gp0_7.into_output().expect("Pin 0.7 failed");
+    // let mut pin1_0 = pins.gp1_0.into_output().expect("Pin 1.0 failed");
+    // let mut pin1_1 = pins.gp1_1.into_output().expect("Pin 1.1 failed");
+    // let mut pin1_2 = pins.gp1_2.into_output().expect("Pin 1.2 failed");
+    // let mut pin1_3 = pins.gp1_3.into_output().expect("Pin 1.3 failed");
+    // let mut pin1_4 = pins.gp1_4.into_output().expect("Pin 1.4 failed");
+    // let mut pin1_5 = pins.gp1_5.into_output().expect("Pin 1.4 failed");
     defmt::info!("All pins initialized");
     Timer::after_secs(1).await;
 
     loop {
-        pin0_0.set_high().expect("Pin 0.0 failed");
-        pin0_1.set_high().expect("Pin 0.1 failed");
-        pin0_2.set_high().expect("Pin 0.2 failed");
-        pin0_3.set_high().expect("Pin 0.3 failed");
-        pin0_4.set_high().expect("Pin 0.4 failed");
-        pin0_5.set_high().expect("Pin 0.5 failed");
-        pin0_6.set_high().expect("Pin 0.6 failed");
-        pin0_7.set_high().expect("Pin 0.7 failed");
-        pin1_0.set_high().expect("Pin 1.0 failed");
-        pin1_1.set_high().expect("Pin 1.1 failed");
-        pin1_2.set_high().expect("Pin 1.2 failed");
-        pin1_3.set_high().expect("Pin 1.3 failed");
-        pin1_4.set_high().expect("Pin 1.4 failed");
-        // // With this delay, the program does not crash
-        // Timer::after(Duration::from_millis(1)).await;
-        pin1_5.set_high().expect("Pin 1.5 failed");
+        pins.gp0_0.enable_pull_up(true).expect("failed");
+        pins.gp0_1.enable_pull_up(true).expect("failed");
+        pins.gp0_2.enable_pull_up(true).expect("failed");
+        pins.gp0_3.enable_pull_up(true).expect("failed");
+        pins.gp0_4.enable_pull_up(true).expect("failed");
+        pins.gp0_5.enable_pull_up(true).expect("failed");
+        pins.gp0_6.enable_pull_up(true).expect("failed");
+        pins.gp0_7.enable_pull_up(true).expect("failed");
+        pins.gp1_0.enable_pull_up(true).expect("failed");
+        pins.gp1_1.enable_pull_up(true).expect("failed");
+        pins.gp1_2.enable_pull_up(true).expect("failed");
+        pins.gp1_3.enable_pull_up(true).expect("failed");
+        pins.gp1_4.enable_pull_up(true).expect("failed");
+        pins.gp1_5.enable_pull_up(true).expect("failed");
+
         defmt::info!("Sett all pins high");
-        Timer::after(Duration::from_millis(400)).await;
+        Timer::after(Duration::from_millis(1000)).await;
 
-        pin0_0.set_low().expect("Pin 0.0 failed");
-        pin0_1.set_low().expect("Pin 0.1 failed");
-        pin0_2.set_low().expect("Pin 0.2 failed");
-        pin0_3.set_low().expect("Pin 0.3 failed");
-        pin0_4.set_low().expect("Pin 0.4 failed");
-        pin0_5.set_low().expect("Pin 0.5 failed");
-        pin0_6.set_low().expect("Pin 0.6 failed");
-        pin0_7.set_low().expect("Pin 0.7 failed");
-        pin1_0.set_low().expect("Pin 1.0 failed");
-        pin1_1.set_low().expect("Pin 1.1 failed");
-        pin1_2.set_low().expect("Pin 1.2 failed");
-        pin1_3.set_low().expect("Pin 1.3 failed");
-        pin1_4.set_low().expect("Pin 1.4 failed");
-        // // With this delay, the program does not crash
-        // Timer::after(Duration::from_millis(1)).await;
-        pin1_5.set_low().expect("Pin 1.5 failed");
-
+        pins.gp0_0.enable_pull_up(false).expect("failed");
+        pins.gp0_1.enable_pull_up(false).expect("failed");
+        pins.gp0_2.enable_pull_up(false).expect("failed");
+        pins.gp0_3.enable_pull_up(false).expect("failed");
+        pins.gp0_4.enable_pull_up(false).expect("failed");
+        pins.gp0_5.enable_pull_up(false).expect("failed");
+        pins.gp0_6.enable_pull_up(false).expect("failed");
+        pins.gp0_7.enable_pull_up(false).expect("failed");
+        pins.gp1_0.enable_pull_up(false).expect("failed");
+        pins.gp1_1.enable_pull_up(false).expect("failed");
+        pins.gp1_2.enable_pull_up(false).expect("failed");
+        pins.gp1_3.enable_pull_up(false).expect("failed");
+        pins.gp1_4.enable_pull_up(false).expect("failed");
+        pins.gp1_5.enable_pull_up(false).expect("failed");
         defmt::info!("Sett all pins low");
-        Timer::after(Duration::from_millis(200)).await;
+        Timer::after(Duration::from_millis(1000)).await;
     }
 }
